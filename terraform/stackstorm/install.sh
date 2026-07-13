50871741e3ab8fdc0531385ef5f4895c824f1aa4
Martez Reed <martez.reed@greenreedtech.com>
2026-07-12 23:49:20 -0500

    Update code

--- terraform/stackstorm/install.sh
+++ terraform/stackstorm/install.sh
@@ -11,3 +11,30 @@ EOF
 st2 pack install https://github.com/martezr/nightlight-lab.git
 
 sudo st2ctl reload --register-configs
+
+sudo mkdir -p /stackstorm
+sudo chmod -R 777 /stackstorm
+
+# Install Go
+GO_VERSION=1.24.5
+curl -fsSL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o /tmp/go.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+export PATH="/usr/local/go/bin:$PATH"
+echo 'export PATH="/usr/local/go/bin:$PATH"' >> ~/.bashrc
+
+# Build the nightlight Terraform provider and configure dev_overrides
+GOBIN="$(go env GOPATH)/bin"
+
+git clone https://github.com/martezr/terraform-provider-nightlight.git /tmp/terraform-provider-nightlight
+cd /tmp/terraform-provider-nightlight
+go install .
+
+cat <<EOF > ~/.terraformrc
+provider_installation {
+  dev_overrides {
+    "martezr/nightlight" = "${GOBIN}"
+  }
+  direct {}
+}
+EOF
