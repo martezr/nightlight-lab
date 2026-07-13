864b457d5fad0c913d3ac3b260697e22cdbf27a7
Martez Reed <martez.reed@greenreedtech.com>
2026-07-13 15:02:00 -0500

    Update salt initialization

--- terraform/hyperv_cluster/instance.tf
+++ terraform/hyperv_cluster/instance.tf
@@ -1,3 +1,15 @@
+variable "instance_count" {
+  description = "Number of instances to deploy"
+  type        = number
+  default     = 2
+}
+
+variable "instance_name_prefix" {
+  description = "Prefix for instance names"
+  type        = string
+  default     = "hyperv"
+}
+
 data "nightlight_site" "example" {
   name = "us-east-1"
 }
@@ -15,11 +27,12 @@ data "nightlight_image" "windows2016" {
 }
 
 output "ip_addresses" {
-  value = nightlight_instance.hyperv01.guest_ip_addresses
+  value = { for k, v in nightlight_instance.hyperv : k => v.guest_ip_addresses }
 }
 
-resource "nightlight_instance" "hyperv01" {
-  name           = "hyperv01"
+resource "nightlight_instance" "hyperv" {
+  count          = var.instance_count
+  name           = format("%s%02d", var.instance_name_prefix, count.index + 1)
   cpu_cores      = 4
   cpu_sockets    = 2
   memory_mb      = 8192
@@ -34,17 +47,24 @@ resource "nightlight_instance" "hyperv01" {
     {
       index_number = 0
       boot_order   = 1
-      size_gb      = 100
+      size_gb      = 65
       bus_type     = "virtio"
       datastore_id = "defaultdatastore"
       existing_path = data.nightlight_image.windows2016.path
+    },
+    {
+      index_number = 0
+      boot_order   = 2
+      size_gb      = 100
+      bus_type     = "virtio"
+      datastore_id = "defaultdatastore"
     }
   ]
 
   network_interfaces = [
     {
       index_number = 0
-      boot_order   = 2
+      boot_order   = 3
       bridge_name  = data.nightlight_site.example.bridges[0]
       subnet_id    = data.nightlight_subnet.management.id
       model        = "virtio"
